1b955346f9ead215152cbe1036721642e23395b6
Ian White <ianwhite@utexas.edu>
2014-03-10 15:13:44 -0500

    test for flash persistence on subsequent requests. Test passes with flash.now in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       # Sign the user in and redirect to the user's show page
     else
       #create an error msg and re-render the signin form
-      flash[:error] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
   end
