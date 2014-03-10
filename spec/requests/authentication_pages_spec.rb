5c05f6aa841422ec27f3f3b6712424d128d3e89e
Ian White <ianwhite@utexas.edu>
2014-03-10 14:41:54 -0500

    test for signin failure

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,4 +9,11 @@ describe "Authentication" do
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
   end
+
+  describe "sign in" do
+    before { visit signin_path }
+
+    it { should have_title('Sign in') }
+    it { should have_selector('div.alert.alert-error') }
+  end
 end
