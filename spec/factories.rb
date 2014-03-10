c9c454bb30360e2b649ea329a01db67679e0a9ea
Ian White <ianwhite@utexas.edu>
2014-03-10 00:16:20 -0500

    failing profile page test spec

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Ian White"
+    email    "thisguy@users.com"
+    password "suckit"
+    password_confirmation "suckit"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,6 +13,7 @@ describe "UserPages" do
 
   describe "profile page" do
     # Replace with code to make a user variable
+    let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
