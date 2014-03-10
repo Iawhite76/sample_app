c8e9e6a2fad9876ba887e657d2da98f7100c1dbe
Ian White <ianwhite@utexas.edu>
2014-03-09 21:49:36 -0500

    implement password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                                               uniqueness: { case_senstive: false }
+  # has_secure_password
 end
 
